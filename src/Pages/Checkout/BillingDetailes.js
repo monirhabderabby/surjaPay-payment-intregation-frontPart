150222875b21182f9bc27dff6ba3442cdf1d3a8d
Monir <monirhrabby.personal@gmail.com>
2023-04-01 07:59:21 +0600

    setup react hook form and validate billingDetailes Form

--- src/Pages/Checkout/BillingDetailes.js
+++ src/Pages/Checkout/BillingDetailes.js
@@ -1,51 +1,68 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export const BillingDetailes = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+    };
     return (
         <div className="w-full max-w-[600px] h-auto shadow-[0px_2px_6px_rgba(0,0,0,0.14)] p-[20px] rounded-[4px] bg-white">
             <h3 className="text-[#000000] font-bold font-sans text-[16px]">Billing Address</h3>
-            <div className="w-full grid grid-cols-2 gap-x-[18px] gap-y-[16px] mt-[20px]">
-                <input
-                    type="text"
-                    id="name"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Full Name"
-                />
-                <input
-                    type="email"
-                    id="phone"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Email"
-                />
-                <input
-                    type="text"
-                    id="address"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Address"
-                />
-                <input
-                    type="text"
-                    id="city"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="City"
-                />
-                <input
-                    type="text"
-                    id="phone"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Phone"
-                />
 
-                <input
-                    type="text"
-                    id="postalCode"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="postal Code"
-                />
-                <button className="w-full col-span-2 bg-gradient-to-r from-[rgba(0,243,161,.8)] to-[rgba(0,243,161,0.4)] rounded-[4px] hover:bg-[rgba(0,243,161,.7)] h-[40px] duration-150">
-                    Pay Now
-                </button>
-            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="w-full grid grid-cols-2 gap-x-[18px] gap-y-[16px] mt-[20px]">
+                    <input
+                        type="text"
+                        id="name"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Full Name"
+                        {...register("customer_name")}
+                    />
+                    <input
+                        type="email"
+                        id="phone"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Email"
+                        {...register("customer_email")}
+                    />
+                    <input
+                        type="text"
+                        id="address"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Address"
+                        {...register("customer_address")}
+                    />
+                    <input
+                        type="text"
+                        id="city"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="City"
+                        {...register("customer_city")}
+                    />
+                    <input
+                        type="text"
+                        id="phone"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Phone"
+                        {...register("customer_phone")}
+                    />
+
+                    <input
+                        type="text"
+                        id="postalCode"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="postal Code"
+                        {...register("customer_postcode")}
+                    />
+                    <input
+                        type="submit"
+                        value="Pay Now"
+                        className="w-full col-span-2 bg-gradient-to-r from-[rgba(0,243,161,.8)] to-[rgba(0,243,161,0.4)] rounded-[4px] hover:bg-[rgba(0,243,161,.7)] h-[40px] duration-150"
+                    />
+                </div>
+            </form>
         </div>
     );
 };
